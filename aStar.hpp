502106da6eddb018c3e825471587e1d63a8a8575
kurczak500 <dwaksmundzki@interia.pl>
2018-04-21 10:59:05 +0200

    clear all

--- aStar.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef TESTASTAR_H_INCLUDED
-#define TESTASTAR_H_INCLUDED
-
-#include <string>
-
-std::string FindPath(Enemy **enemy, Wall **wall, Player * player, short choice, bool onlyImmortal, bool isPlayer);
-
-#endif
